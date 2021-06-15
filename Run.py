abcd35e203a62f353bbaa518d663654c2cc744cc
M HARIS REHMAN <66814375+mharisrehman@users.noreply.github.com>
2021-06-15 05:36:58 -0700

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,10 @@
+
+import SEBAL
+
+inputExcel =  r"E:\GCICS\SEBAL\Excel_SEBAL_v3_4_4.xlsx"
+
+for number in range(7,9):
+    SEBAL.pySEBAL.pySEBAL_code.main(number,inputExcel)
+
+
+
---
+++ __init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+
+Description:
+This module contains a compilation of scripts and functions to run pySEBAL
+"""
+
+
+from SEBAL import pySEBAL
+
+__all__ = ['pySEBAL']
+
+__version__ = '0.1'
